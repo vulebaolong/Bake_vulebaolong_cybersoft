0779c4ed0d82b6a71369f90d7f4b7000b4e9d996
unknown <vulebaolong@gmail.com>
2022-09-22 09:34:00 +0700

    done3

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-function backtotop(params1,params2) {
-    var top =  document.querySelector(params1)
+function backtotop(params1, params2) {
+    var top = document.querySelector(params1)
     var body = document.querySelector('body')
     body.onscroll = (e) => {
-        console.log(window.scrollY);
+        //console.log(window.scrollY);
         if (window.scrollY >= 200) {
             if (top.classList.length > 0) {
                 if (!Array.from(top.classList).includes(params2)) {
@@ -18,4 +18,17 @@ function backtotop(params1,params2) {
             }
         }
     }
+}
+
+function active(parent, children) {
+    var parent = document.querySelector(parent)
+    parent.querySelectorAll('a').forEach((e) => {
+        e.onclick = (e) => {
+            // parent.querySelectorAll(children).forEach((e) => {
+            //     e.classList.remove('active')
+            // })
+            parent.querySelector('.active').classList.remove('active')
+            e.target.classList.add('active')
+        }
+    })
 }
